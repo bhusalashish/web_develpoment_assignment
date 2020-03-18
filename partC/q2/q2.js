1c9bba29b84a13ca1d62dec81b1da67b1c97ec04
Ashish <ashsh.bhusal.16999@gmail.com>
2020-03-19 00:20:56 +0530

    PartC Question 2

--- partC/q2/q2.js
+++ partC/q2/q2.js
@@ -3,3 +3,20 @@
 // Here 46 is the amount. and 25, 10, 5, 2, 1 are coins. 
 // Output : 25, 10, 10, 1
 
+function coinChange(val, arr){
+    let n = arr.length;
+    if(val==0 || n==0) return [];
+    let res = [];
+    arr.sort(function(a,b){
+        return b-a;
+    });
+    for(let i=0; i<n; i++){
+        while(val>=arr[i]){
+            val-=arr[i];
+            res.push(arr[i]);
+        }
+    }
+    return res;
+}
+
+console.log(coinChange(93, [1, 2, 5, 10, 20, 50, 100, 500, 1000]));
